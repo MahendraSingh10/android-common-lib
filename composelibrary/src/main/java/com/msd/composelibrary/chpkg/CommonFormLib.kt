3868c3b7a7361f32ecb2002813d794efcb69e102
MahendraSingh10 <148310827+MahendraSingh10@users.noreply.github.com>
2024-05-10 19:02:09 +0530

    1.1
    1.1

--- composelibrary/src/main/java/com/msd/composelibrary/chpkg/CommonFormLib.kt
+++ composelibrary/src/main/java/com/msd/composelibrary/chpkg/CommonFormLib.kt
@@ -1,8 +1,11 @@
 package com.msd.composelibrary.chpkg
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,13 +13,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,6 +36,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.text.style.TextOverflow
 
 @Composable
 fun generateForm(context: Context,
@@ -125,4 +137,71 @@ fun generateForm(context: Context,
         }
     }
 
+}
+
+
+
+@Composable
+fun HelloFunction(context:Context){
+    Toast.makeText(context, "",Toast.LENGTH_SHORT)
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TodoView(vm: TodoViewModel, onBackButtonClick: () -> Unit = {}) {
+
+    LaunchedEffect(Unit, block = {
+        vm.getTodoList()
+    })
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Row {
+                    Text("Todos")
+                    Button(onClick = {
+                        onBackButtonClick()
+                    }) {
+                        Text("Click to Back")
+                    }
+                }
+            })
+        },
+        content = {
+            if(vm.errorMessage.isEmpty()){
+                Column(modifier = Modifier.padding(15.dp)) {
+                    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                        items(vm.todoList){
+                                todo ->
+                            Column {
+                                Row(modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(5.dp),
+                                    horizontalArrangement = Arrangement.SpaceBetween) {
+                                    Box(   modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(0.dp, 0.dp, 16.dp, 0.dp) ){
+                                        Checkbox(checked = todo.completed, onCheckedChange = null)
+                                        Spacer(modifier = Modifier.width(16.dp))
+                                        Text(
+                                            modifier = Modifier.padding(start = 10.dp),
+                                            text = todo.title, maxLines = 2,
+                                            overflow = TextOverflow.Ellipsis)
+
+
+                                    }
+                                    Divider()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Text(vm.errorMessage)
+            }
+        }
+    )
 }
--- composelibrary/src/main/java/com/msd/composelibrary/chpkg/Todo.kt
+++ composelibrary/src/main/java/com/msd/composelibrary/chpkg/Todo.kt
@@ -1,31 +1,8 @@
 package com.msd.composelibrary.chpkg
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
-import android.annotation.SuppressLint
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ExperimentalMaterial3Api
 
 data class Todo(
     var userId: Int,
@@ -54,64 +31,5 @@ interface APIService {
 
 
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TodoView(vm: TodoViewModel, onBackButtonClick: () -> Unit = {}) {
-
-    LaunchedEffect(Unit, block = {
-        vm.getTodoList()
-    })
-
-    Scaffold(
-        topBar = {
-            TopAppBar(title = {
-                Row {
-                    Text("Todos")
-                    Button(onClick = {
-                        onBackButtonClick()
-                    }) {
-                        Text("Click to Back")
-                    }
-                }
-            })
-        },
-        content = {
-            if(vm.errorMessage.isEmpty()){
-                Column(modifier = Modifier.padding(15.dp)) {
-                    LazyColumn(modifier = Modifier.fillMaxWidth()) {
-                        items(vm.todoList){
-                                todo ->
-                            Column {
-                                Row(modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(5.dp),
-                                    horizontalArrangement = Arrangement.SpaceBetween) {
-                                    Box(   modifier = Modifier
-                                        .fillMaxWidth()
-                                        .padding(0.dp, 0.dp, 16.dp, 0.dp) ){
-                                        Checkbox(checked = todo.completed, onCheckedChange = null)
-                                        Spacer(modifier = Modifier.width(16.dp))
-                                        Text(
-                                            modifier = Modifier.padding(start = 10.dp),
-                                            text = todo.title, maxLines = 2,
-                                            overflow = TextOverflow.Ellipsis)
-
-
-                                    }
-                                    Divider()
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                Text(vm.errorMessage)
-            }
-        }
-    )
-}
 
 
